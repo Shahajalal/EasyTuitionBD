b10d46c99fda93b3612fe369a02c28f92a05e0c4
tuhin-cse <tuhinabdurrazzak@gmail.com>
2018-10-01 00:31:22 +0600

    Login added

--- app/src/main/java/stsoftwaresolution/easytuitionbd/MainActivity.java
+++ app/src/main/java/stsoftwaresolution/easytuitionbd/MainActivity.java
@@ -2,6 +2,8 @@ package stsoftwaresolution.easytuitionbd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.login) {
+            ///login button work
+
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
